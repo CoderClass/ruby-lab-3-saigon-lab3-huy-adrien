dc6060c6781368c6014d1dd3d10857174c8116a8
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-26 20:44:21 +0700

    Milestone 4: finish

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".alphabetical" do
+    it "returns [] when there are no products" do
+      Product.delete_all
+      products = Product.alphabetical
+      expect(products).to eq []
+    end
+
+    it "returns [a] when there is only one product a" do
+      a = Product.create!(name: 'test product')
+      products = Product.alphabetical
+      expect(products).to eq [a]
+    end
+
+    it "returns [a,b,c] after I create 3 products c, b, a" do
+      c = Product.create!(name: 'c product')
+      b = Product.create!(name: 'b product')
+      a = Product.create!(name: 'a product')
+      products = Product.alphabetical
+      expect(products).to eq [a, b, c]
+    end
+  end
 end
